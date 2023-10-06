1294854aad2802eaeb817281d5196c7a8b8c1964
ShaheerKz <shariikhan925@gmil.com>
2023-10-06 16:28:15 -0700

    feat: home-page

--- index.php
+++ index.php
@@ -20,18 +20,70 @@
         <div class="home-banner">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-3">
+                    <div class="col-lg-3">
                         <div class="frame-area">
-                            <div class="frame-img position-relative">
-                                <img src="./assets/images/frame-aside.png" alt="">
-                                <div class="frame-text">
-                                    <h1 class="frame-head">SAINT OF THE DAY</h1>
-                                    <i class="fa-solid fa-quote-right frame-icon"></i>
-                                    <p class="frame-content">We can't help everyone, but everyone can help someone</p>
-                                    <h3 class="frame-bottom">AESOP</h3>
-                                    <a href="#" class="frame-btn">Read More</a>
+                            <div class="frame-text">
+                                <h1 class="frame-head">SAINT OF THE DAY</h1>
+                                <i class="fa-solid fa-quote-right frame-icon"></i>
+                                <p class="frame-content">We can't help everyone, but everyone can help someone</p>
+                                <h3 class="frame-bottom">AESOP</h3>
+                                <a href="#" class="frame-btn">Read More</a>
+                            </div>
+                            <div class="frame-text">
+                                <h1 class="frame-head">POEM OF THE WEEK</h1>
+                                <h3 class="head-bottom">THE POINT NO RETURN</h3>
+                                <p class="frame-content">It is a long established fact that a reader will be distracted by the readable content</p>
+                                <a href="#" class="frame-btn">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="window-wrapper">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="top-window text-center">
+                                        <img src="./assets/images/top-window.png" class="img-fluid w-100 " alt="">
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="top-window text-center">
+                                        <img src="./assets/images/top-window-right.png" class="img-fluid w-100" alt="">
+                                    </div>
                                 </div>
                             </div>
+                            <div class="row py-5">
+                                <div class="col-3">
+                                    <div class="window-bottom">
+                                        <img src="./assets/images/img-1.png" class="img-fluid w-100" alt="">
+                                    </div>
+                                </div>
+                                <div class="col-3">
+                                    <div class="window-bottom">
+                                        <img src="./assets/images/img-3.png" class="img-fluid w-100" alt="">
+                                    </div>
+                                </div>
+                                <div class="col-3">
+                                    <div class="window-bottom">
+                                        <img src="./assets/images/img-3.png" class="img-fluid w-100" alt="">
+                                    </div>
+                                </div>
+                                <div class="col-3">
+                                    <div class="window-bottom">
+                                        <img src="./assets/images/img-4.png" class="img-fluid w-100" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="main-left">
+                            <div class="left-side-wrapper d-flex justify-content-center">
+                                <div class="img-border">
+                                    <img src="./assets/images/img-frame.PNG" class="img-fluid" alt="">
+                                    <div class="bottom"></div>
+                                </div>
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
